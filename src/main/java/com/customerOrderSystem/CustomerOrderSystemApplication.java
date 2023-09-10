051c8dd92ba12a01639e4e9a0fcd5d7d1fbc0b18
saraabramovitz <sh0527685990@gmail.com>
2023-09-10 17:39:30 +0300

    second commit

---
+++ src/main/java/com/customerOrderSystem/CustomerOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.customerOrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOrderSystemApplication.class, args);
+	}
+
+}
